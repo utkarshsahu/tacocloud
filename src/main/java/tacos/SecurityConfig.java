426c8ffa99e343879154d826b2e009eb348872e4
utkarshsahu <utkarshsahu@hotmail.com>
2019-06-02 19:15:52 +0530

    JDBC backed security store

--- src/main/java/tacos/SecurityConfig.java
+++ src/main/java/tacos/SecurityConfig.java
@@ -1,9 +1,14 @@
 package tacos;
 
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,16 +16,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder enc = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication()
-			.withUser("thanos")
-				.password(enc.encode("infinitystone"))
-				.authorities("ROLE_USER")
-			.and()
-			.withUser("ironman")
-				.password(enc.encode("3000"))
-				.authorities("ROLE_USER");
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery("")
+			.authoritiesByUsernameQuery("")
+			.passwordEncoder(new BCryptPasswordEncoder(-1));
 	}
 }
